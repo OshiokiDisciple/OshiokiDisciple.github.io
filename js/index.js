457db9a2ee4f6d958eb5e4840242850d964967c3
OshiokiDisciple <57633536+OshiokiDisciple@users.noreply.github.com>
2020-04-23 00:10:12 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 $(function() {
-  /ハンバーガーボタンをクリックしたときの処理/
-  $(".btn-gnavi").on("click", function() {
-
-  })
-}
+  $(".hbg-btn").click(function() {
+    $(".hbg-line-1,.hbg-line-2,.hbg-line-3")
+    .toggleClass("open");})
+});
